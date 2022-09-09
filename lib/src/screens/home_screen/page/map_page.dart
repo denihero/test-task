35fdf3135358fb4016756fab8cd01acbc4b4fa34
denihero <denihero81@gmail.com>
2022-09-09 15:17:15 +0600

    add my location button

--- lib/src/screens/home_screen/page/map_page.dart
+++ lib/src/screens/home_screen/page/map_page.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
-class MapPage extends StatelessWidget {
-   MapPage({Key? key}) : super(key: key);
+class MapPage extends StatefulWidget {
+   const MapPage({Key? key}) : super(key: key);
 
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
   late GoogleMapController mapController;
 
   final LatLng _center = const LatLng(420.5937, 78.9629);
+
   final Location _location = Location();
 
   void _onMapCreated(GoogleMapController controller) {
@@ -19,18 +25,39 @@ class MapPage extends StatelessWidget {
     });
   }
 
+   Future<LatLng?> getUserLocation() async {
+     LocationData? currentLocation;
+     final location = Location();
+     try {
+       currentLocation = await location.getLocation();
+       final lat = currentLocation.latitude;
+       final lng = currentLocation.longitude;
+       final center = LatLng(lat!, lng!);
+       return center;
+     } on Exception {
+       currentLocation = null;
+       return null;
+     }
+   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: false,
+        mapType: MapType.normal,
         onMapCreated: _onMapCreated,
-        myLocationButtonEnabled: true,
         myLocationEnabled: true,
         initialCameraPosition: CameraPosition(
           target: _center,
           zoom: 11.0,
-
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        onPressed: getUserLocation,
+        child: const Icon(Icons.location_on,color: Colors.black,),
       ),
     );
   }
